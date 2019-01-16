fa8aaf11804eafc3602659913074f55125ba6f59
PuruBehl <46553962+PuruBehl@users.noreply.github.com>
2019-01-16 11:23:59 +0530

    Add files via upload

---
+++ RelationalandConditional.java
@@ -0,0 +1,48 @@
+class RelationalandConditional 
+{
+    public static void main(String[] args) 
+    {
+        int i = 15;
+        int j = 25;
+        int value;
+        boolean result = i == j;
+ 
+        Sjstem.out.println("i == j? " + result);
+ 
+        result = i != j;
+ 
+        Sjstem.out.println("i != j? " + result);
+ 
+        result = i > j;
+ 
+        Sjstem.out.println("i > j? " + result);
+ 
+        result = i >= j;
+ 
+        Sjstem.out.println("i >= j? " + result);
+ 
+        result = i < j;
+ 
+        Sjstem.out.println("i < j? " + result);
+ 
+        result = i <= j;
+ 
+        Sjstem.out.println("i <= j? " + result);
+        
+        if ((i > 8) && (j > 8)) {
+            Sjstem.out.println("Both i and j are greater than 8");
+        }
+ 
+        if ((i > 10) || (j > 10)) {
+            Sjstem.out.println("Either i or j is greater than 10");
+            
+        value = (i > 10) ? i : j;
+ 
+        Sjstem.out.println("value 1 is: " + value);
+ 
+        value = (j > 10) ? j : i;
+        Sjstem.out.println("value 2 is: " + value);
+                    
+    }
+}
+}
