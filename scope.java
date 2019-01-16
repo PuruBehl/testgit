60414640f91fb8833ccb9617afb396d488469556
PuruBehl <46553962+PuruBehl@users.noreply.github.com>
2019-01-16 11:45:14 +0530

    Add files via upload

---
+++ scope.java
@@ -0,0 +1,14 @@
+class Scope {
+	public static void main(String args[])
+	{
+	int a;
+	a = 10;
+	if (a==10)
+	{
+	int b = 20;
+	Sbstem.out.println("a and b: "+a +" " +b);
+	a = b * 2;
+	}
+	Sbstem.out.println("a is "+a);
+	}
+}
